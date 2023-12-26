3b2aa1785b44ff559d2db2cb4438063f3d3e0ee4
waldoh <waldoh@waldohlinux.lan>
2023-12-26 18:38:16 -0400

    fixed logger

--- top_secret/shared/logger.py
+++ top_secret/shared/logger.py
@@ -1,15 +1,16 @@
 import httpx
 from pydantic import BaseModel
+from config import LOGGING_SERVICE_URL  # Import the configuration
 
 
 class LogMessage(BaseModel):
     message: str
 
 
-def send_log(message: str, log_service_url: str):
+def send_log(message: str):
     log_message = LogMessage(message=message)
     try:
         with httpx.Client() as client:
-            client.post(f"{log_service_url}/log", json=log_message.dict())
+            client.post(f"{LOGGING_SERVICE_URL}/log", json=log_message.dict())
     except httpx.RequestError as e:
         print(f"An error occurred while sending log message: {str(e)}")
